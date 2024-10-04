d1df7959ed0be06eb3519a1fdd6d9cc6a04e9be4
Ryan G Sands <rgsands@clemson.edu>
2024-10-04 16:48:28 +0000

    Added 2 files to /Sprint3/Backend/Controllers

---
+++ Sprint3/Backend/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost("logout")]
+    [Authorize]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return Ok(new { message = "Logged out successfully" });
+    }
+}
