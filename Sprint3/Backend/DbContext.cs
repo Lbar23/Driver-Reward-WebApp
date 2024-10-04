9bd6e176b056e2de178f8c124b0aec508c4a1a41
Ryan G Sands <rgsands@clemson.edu>
2024-10-04 16:46:43 +0000

    Added 8 files to /Sprint3/Backend

---
+++ Sprint3/Backend/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Define DbSet properties for your entities here
+    // public DbSet<YourEntity> YourEntities { get; set; }
+    public DbSet<User> Users { get; set; }
+
+}
