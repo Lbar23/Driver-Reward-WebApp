b16873a8b5ddf8e470a0f416bd7195d802abfc98
TolerateLess-wav <123459028+TolerateLess-wav@users.noreply.github.com>
2024-10-21 14:54:18 -0400

    Logging system framework implemented w/ (to date) updated packages

--- Project/Backend_Server/Program.cs
+++ Project/Backend_Server/Program.cs
@@ -8,118 +8,142 @@ using Backend_Server.Services;
 using Amazon;
 using Amazon.SecretsManager;
 using Amazon.Extensions.NETCore.Setup;
+using Serilog;
+
 
 var builder = WebApplication.CreateBuilder(args);
+try {
+    // AWS Secrets Manager setup
+    var awsOptions = new AWSOptions
+    {
+        Region = RegionEndpoint.USEast2,
+    };
 
-// AWS Secrets Manager setup
-var awsOptions = new AWSOptions
-{
-    Region = RegionEndpoint.USEast2,
-};
+    builder.Services.AddAWSService<IAmazonSecretsManager>(awsOptions);
 
-builder.Services.AddAWSService<IAmazonSecretsManager>(awsOptions);
+    builder.Services.AddScoped<DbConnectionProvider>();
 
-builder.Services.AddScoped<DbConnectionProvider>();
+    builder.Services.AddControllers();
 
-builder.Services.AddControllers();
+    // Swagger setup
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Good Driver Incentive Program API", Version = "v2" });
+    });
 
-// Swagger setup
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Good Driver Incentive Program API", Version = "v2" });
-});
+    // CORS setup
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowSpecificOrigins",
+            builder =>
+            {
+                builder.WithOrigins("BASE_API_URL", "http://localhost:5041")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+    });
 
-// CORS setup
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins",
-        builder =>
-        {
-            builder.WithOrigins("BASE_API_URL", "http://localhost:5041")
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
-});
+    // Identity Services and Options
+    builder.Services.AddIdentity<Users, IdentityRole<int>>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Tokens.AuthenticatorIssuer = "GitGud";
+        options.Tokens.EmailConfirmationTokenProvider = "email";
+        options.Tokens.ChangePhoneNumberTokenProvider = "phone";
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+        options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        options.User.RequireUniqueEmail = true;
+    })
 
-// Identity Services and Options
-builder.Services.AddIdentity<Users, IdentityRole<int>>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-    options.Tokens.AuthenticatorIssuer = "GitGud";
-    options.Tokens.EmailConfirmationTokenProvider = "email";
-    options.Tokens.ChangePhoneNumberTokenProvider = "phone";
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
-    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = true;
-})
+    .AddEntityFrameworkStores<AppDBContext>()
+    .AddDefaultTokenProviders();
 
-.AddEntityFrameworkStores<AppDBContext>()
-.AddDefaultTokenProviders();
+    // Add cookie authentication
+    builder.Services.ConfigureApplicationCookie(options =>
+    {
+        // options.LoginPath = "/api/login";  //will come back to later
+        // options.LogoutPath = "/api/logout"; 
+        options.Cookie.HttpOnly = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+        options.SlidingExpiration = true;
+    });
 
-// Add cookie authentication
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    // options.LoginPath = "/api/login";  //will come back to later
-    // options.LogoutPath = "/api/logout"; 
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-    options.SlidingExpiration = true;
-});
+    builder.Services.AddDbContext<AppDBContext>((serviceProvider, options) =>
+    {
+        var dbProvider = serviceProvider.GetRequiredService<DbConnectionProvider>();
+        var connection = dbProvider.GetDbConnectionAsync().Result;
+        options.UseMySql(connection.ConnectionString,
+            ServerVersion.AutoDetect(connection.ConnectionString),
+            mySqlOptions => mySqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null));
+    });
 
-builder.Services.AddDbContext<AppDBContext>((serviceProvider, options) =>
-{
-    var dbProvider = serviceProvider.GetRequiredService<DbConnectionProvider>();
-    var connection = dbProvider.GetDbConnectionAsync().Result;
-    options.UseMySql(connection.ConnectionString,
-        ServerVersion.AutoDetect(connection.ConnectionString),
-        mySqlOptions => mySqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null));
-});
+    // Automated Backup Service
+    builder.Services.AddHostedService<BackupService>();
 
-// Automated Backup Service
-builder.Services.AddHostedService<BackupService>();
+    // Adds static files to root Backend
+    builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = "wwwroot");
 
-// Adds static files to root Backend
-builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = "wwwroot");
+    // Basic Serilog Service build
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .CreateLogger();
 
-var app = builder.Build();
+    builder.Host.UseSerilog();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Good Driver Incentive Program API v2"));
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Good Driver Incentive Program API v2"));
+    }
+    else
+    {
+        app.UseHsts();
+    }
+
+    app.UseStaticFiles();
+    app.UseSpaStaticFiles();
+
+    app.UseRouting();
+
+    app.UseCors("AllowSpecificOrigins");
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    //SPA route
+    app.UseSpa(spa =>
+    {
+        spa.Options.SourcePath = "wwwroot";
+    });
+
+    app.MapFallbackToFile("/index.html");
+
+    // Enable Serilog Request Logging for API requests
+    app.UseSerilogRequestLogging();
+
+    Log.Information("Starting Web Host...");
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseHsts();
+    Log.Error(ex, "The web server terminated unexpectedly.");
+    throw;
 }
-
-app.UseStaticFiles();
-app.UseSpaStaticFiles();
-
-app.UseRouting();
-
-app.UseCors("AllowSpecificOrigins");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-//SPA route
-app.UseSpa(spa =>
+finally
 {
-    spa.Options.SourcePath = "wwwroot";
-});
-
-app.MapFallbackToFile("/index.html");
-
-app.Run();
+    Log.CloseAndFlush();
+}
