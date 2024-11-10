174d9e2128e47b0502b001717c4b58e6208c600c
RyGSan <rgsands@clemson.edu>
2024-11-10 12:23:22 -0500

    Minor changes to FAQ. Could have more items, but wasnt sure how some things would be implemented yet.

--- Project/frontend_client/src/pages/FAQ.tsx
+++ Project/frontend_client/src/pages/FAQ.tsx
@@ -31,27 +31,27 @@ const FAQ: React.FC = () => {
   const faqs: FAQData[] = [
     {
       question: "How do I create an account?",
-      answer: "To create an account, click on the 'Create New Account' link on the login page. Fill in your details and follow the prompts to complete registration."
+      answer: "To create an account, click on the 'Sign Up' Button, and from there you will become a guest."
     },
     {
       question: "What are the password requirements?",
-      answer: "We recommend using a strong password that is at least 8 characters long, includes uppercase and lowercase letters, numbers, and special characters."
+      answer: "To create a password, it must have a number, a lowercase letter, an uppercase letter, be at least 8 characters long and no characters can be repeated."
     },
     {
       question: "How can I reset my password?",
-      answer: "If you've forgotten your password, click on the 'Forgot Password' link on the login page. Enter your email address, and we'll send you instructions to reset your password."
+      answer: "If you've forgotten your password, click on the 'Forgot Password' link on the login page."
     },
     {
       question: "How do I earn points?",
-      answer: "You can earn points by driving safely and completing objectives set by your sponsor."
+      answer: "To earn points, first you must be associated with at least one sponsor. From there, simply follow instructions from your sponsor to earn points."
     },
     {
       question: "How do I apply to a sponsor?",
-      answer: "To apply to a sponsor, fill out the sponsor application form."
+      answer: "To apply to be a sponsor, simply reach out to us and we will create an account for you if you meet our requirements."
     },
     {
       question: "What can I spend my points on?",
-      answer: "Points can be exchanged for purchases from temp not sure where yet."
+      answer: "You can exchange your points for a variety of items from your sponsors catalog. Each of our sponsors has a unique selection of items."
     }
   ];
 
