112d42f8ea34ff8795af8bfa6df92a305a788276
Damon Milford II <dlmilfo@clemson.edu>
2024-11-18 19:27:55 -0500

    minute or major database updates

--- Project/Backend_Server/AppDBContext.cs
+++ Project/Backend_Server/AppDBContext.cs
@@ -21,6 +21,7 @@ namespace Backend_Server
         public DbSet<Admins> Admins { get; set; }
         public DbSet<SponsorDrivers> SponsorDrivers { get; set; }
         public DbSet<FeedbackForms> FeedbackForms { get; set; }
+        public DbSet<SponsorUsers> SponsorUsers { get; set;} 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -116,13 +117,14 @@ namespace Backend_Server
                 entity.HasOne<Users>()
                     .WithMany()
                     .HasForeignKey(d => d.UserID);
-
-                // entity.HasOne<Sponsors>()
-                //     .WithMany()
-                //     .HasForeignKey(d => d.SponsorID);
-
-                // entity.Property(e => e.TotalPoints)
-                //     .HasDefaultValue(0);
+                
+                entity.Property(e => e.FirstName)
+                    //.IsRequired() uncomment once fully injected
+                    .HasMaxLength(50);
+                
+                entity.Property(e => e.LastName)
+                    //.IsRequired() same here...
+                    .HasMaxLength(50);
             });
 
             modelBuilder.Entity<Products>(entity =>
@@ -301,6 +303,36 @@ namespace Backend_Server
                 entity.Property(e => e.SubmissionDate)
                     .HasColumnType("TIMESTAMP")
                     .HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+
+            modelBuilder.Entity<SponsorUsers>(entity => 
+            {
+                entity.ToTable("SponsorUsers");
+
+                entity.HasKey(su => new { su.SponsorID, su.UserID });
+
+                entity.Property(su => su.IsPrimarySponsor)
+                    .HasDefaultValue(false);
+
+                entity.Property(su => su.JoinDate)
+                    .HasColumnType("TIMESTAMP")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(su => su.SponsorRole)
+                    .HasConversion<string>()
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasDefaultValue(SponsorRole.Standard);
+
+                entity.HasOne(su => su.User)
+                    .WithMany(u => u.SponsorUsers)
+                    .HasForeignKey(su => su.UserID);
+                
+                entity.HasOne(su => su.Sponsor)
+                    .WithMany(s => s.SponsorUsers)
+                    .HasForeignKey(su => su.SponsorID);
+
+                entity.HasIndex(su => new { su.SponsorID, su.IsPrimarySponsor});
             });
         }
     }
--- Project/Backend_Server/Models/Drivers.cs
+++ Project/Backend_Server/Models/Drivers.cs
@@ -8,6 +8,8 @@ namespace Backend_Server.Models
     public class Drivers
     {
         public int UserID { get; set; }
+        public string? FirstName { get; set; } //Change these to required later...and updated DTOs and Controllers
+        public string? LastName { get; set; } //Ditto
         //public int SponsorID { get; set; }
         //public int TotalPoints { get; set; }
         public ICollection<SponsorDrivers> SponsorDrivers { get; set; } = []; //Added after adding bridge entity for connection
