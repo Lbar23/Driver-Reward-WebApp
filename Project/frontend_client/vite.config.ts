a1ae627d8736c9e1568b0e2c0cd50df1be92ff35
Kristen <kaw9@clemson.edu>
2024-11-21 15:13:54 -0500

    config change

--- Project/frontend_client/vite.config.ts
+++ Project/frontend_client/vite.config.ts
@@ -30,9 +30,6 @@ export default defineConfig({
           // Core React libraries
           vendor: ['react', 'react-dom', 'react-router-dom'],
 
-          // // pdf components
-          // pdf: ['jspdf', 'jspdf-autotable'],
-
           // Core MUI components
           'mui-core': [
             '@mui/material/Button',
